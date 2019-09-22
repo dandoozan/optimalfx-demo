113ef8b1dc71f188407da2671d6e26f5f9f7fbdb
Dan Doozan <dandoozan@gmail.com>
2019-09-21 21:10:39 -0700

    Update Header and Description components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from '../Header/Header';
+import Header from './Header';
 import Simulation from '../Simulation/Simulation';
-import Description from '../Description/Description';
+import Description from './Description';
 
 function App() {
   return (
--- src/components/App/Header.tsx
+++ src/components/App/Header.tsx
@@ -4,8 +4,10 @@ import './Header.css';
 export default function Header() {
   return (
     <header className="header">
-      <div className="header__inner-container">
-        <h1>OptimalFX</h1>
+      <div className="header__inner">
+        <h1>
+          OptimalFX <small className="text-muted">Demo</small>
+        </h1>
       </div>
     </header>
   );
