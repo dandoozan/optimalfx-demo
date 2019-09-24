cb00c94ef7e0b11d4377b1388a5dc4d5cb78f785
Dan Doozan <dandoozan@gmail.com>
2019-09-24 09:50:04 -0700

    Make reset button color darker

--- src/components/Simulation/ChartControls.tsx
+++ src/components/Simulation/ChartControls.tsx
@@ -5,7 +5,7 @@ import './ChartControls.css';
 export default function ChartControls(props) {
   return (
     <div className="chart-controls">
-      <Button className="chart-controls__reset" variant="outline-secondary" onClick={props.onReset}>Reset</Button>
+      <Button className="chart-controls__reset" variant="outline-dark" onClick={props.onReset}>Reset</Button>
       <Button className="chart-controls__continue" size="lg" onClick={props.onContinue}>Continue</Button>
     </div>
   );
