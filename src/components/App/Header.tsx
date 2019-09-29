a5d4d96f504ad170517d6efa6a46db418cab8645
Dan Doozan <dandoozan@gmail.com>
2019-09-29 13:38:28 -0700

    Specify styles in terms of "em" instead of "px"

--- src/components/App/Header.tsx
+++ src/components/App/Header.tsx
@@ -5,8 +5,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.innerHeader}>
-        <h1>
-          OptimalFX <small className="text-muted">Demo</small>
+        <h1 className={styles.text}>OptimalFX <small className="text-muted">Demo</small>
         </h1>
       </div>
     </header>
