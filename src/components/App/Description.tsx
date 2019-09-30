d313eb72620255a46173b379a8c8dc2b841f6e3f
Dan Doozan <dandoozan@gmail.com>
2019-09-29 19:24:17 -0700

    Edit description text

--- src/components/App/Description.tsx
+++ src/components/App/Description.tsx
@@ -7,10 +7,10 @@ export default function Description() {
       <p className={styles.paragraph}>
         A trading strategy that compares recent price movements to similar ones
         from the past. A "base pattern" (consisting of two or more
-        recently-completed bars) are matched with similar historical bar
+        recently-completed bars) is matched with similar historical bar
         patterns to find high-confidence trades.
       </p>
-      <p className={styles.paragraph}>See a demo of how it works in the simulation below:</p>
+      <p className={styles.paragraph}>See the simulation below for a demo:</p>
     </div>
   );
 }
